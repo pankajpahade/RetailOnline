4d08c64706f48bcd06fda722f68ddcf736b96ca8
pankajpahade <pankajpahade.92@gmail.com>
2017-06-19 17:16:16 +0530

    marchant added

---
+++ src/main/java/entities/Response.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Response {
+	private String message;
+	private String Status;
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getStatus() {
+		return Status;
+	}
+	public void setStatus(String status) {
+		Status = status; 
+	}
+
+}
